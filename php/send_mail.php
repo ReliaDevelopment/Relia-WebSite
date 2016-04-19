c81b82377a2d6e446eee07998604d5945a4885a3
Martin Aguilera <martin@Martins-MacBook-Pro.local>
2016-04-18 22:51:52 -0500

    Added phpmailer, composer and brand icons

--- php/send_mail.php
+++ php/send_mail.php
@@ -1,26 +1,43 @@
-<?php
-	$to = "jdrendon@imaginacionweb.net"; /*Your Email*/
-	$subject = "Messsage from Jekas Template"; /*Issue*/
-	$date = date ("l, F jS, Y"); 
-	$time = date ("h:i A"); 	
-	$Email=$_REQUEST['Email'];
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Contacto</title>
+		<link rel="stylesheet" href="css/alertify/alertify.rtl.css">
+		<link rel="stylesheet" href="css/alertify/themes/default.rtl.css">
+  </head>
+  <body>
+		<script src="js/alertifyjs/alertify.js"></script>
+		<?php
+			$to = "reliadevelopment@gmail.com"; /*Your Email*/
+			$subject = "Sent from Website"; /*Issue*/
+			$date = date ("l, F jS, Y");
+			$time = date ("h:i A");
+			$Email=$_REQUEST['Email'];
 
-	$msg="
-	Name: $_REQUEST[Name]
-	Email: $_REQUEST[Email]
-	Phone: $_REQUEST[Phone]	
-	
-	Message sent from website on date  $date, hour: $time.\n
+			$msg="
+			Name: $_REQUEST[Name]
+			Email: $_REQUEST[Email]
+			Phone: $_REQUEST[Phone]
 
-	Message:
-	$_REQUEST[message]";
+			Message sent from website on date  $date, hour: $time.\n
 
-	if ($Email=="") {
-		echo "Please enter your email";
-	}
-	else{
-		mail($to, $subject, $msg, "From: $_REQUEST[Email]");
-		echo "Thank you for your message";	
-	}
-	
-?>
+			Message:
+			$_REQUEST[message]";
+
+			if ($Email=="") {
+				alertify.error('Please enter your email');
+			}
+			else{
+				mail($to, $subject, $msg, "From: $_REQUEST[Email]");
+				?>
+				<script type="text/javascript">
+					alertify.success('Mensaje enviado correctamente');
+				</script>
+				<?php
+			}
+		?>
+  </body>
+</html>
